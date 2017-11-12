00e93f247c4be0d9bbffbfa79714c43a4cc1b53b
Jafsari <justinafsari@yahoo.com>
2017-11-12 15:34:15 -0800

    Added functionality to mark favorites

--- hack-n-snooze.js
+++ hack-n-snooze.js
@@ -1,7 +1,7 @@
 $(function() {
 
 });
-
+//When you click on submit button, creates new item for the list
 // listen for a submit event on a form
 $('#new-post').on('submit', function(e) {
 	e.preventDefault() // do not refresh the page
@@ -14,7 +14,13 @@ $('#new-post').on('submit', function(e) {
   if (url.length === 0) return 0
   
 
-  $('ul').append('<li>' + nextTitle + ' ' + '(' + url + ')' + "</li>") 
-
+  $('ol').append('<li><span><i class="fa fa-star-o" aria-hidden="true"></i></span>' + " " +  nextTitle + ' ' + '(' + url + ')' + "</li>") 
+$('span').on('click',function(e){
+	$(this).find($(".fa")).toggleClass('fa fa-star-o fa fa-star');
+	e.stopPropogation()
+});
 
 });
+
+
+
