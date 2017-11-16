41c1282c8af2a580a8f1cb566a9c3069f2469f20
Jafsari <justinafsari@yahoo.com>
2017-11-15 21:56:27 -0800

    Jquery Exercise

--- hack-n-snooze.js
+++ hack-n-snooze.js
@@ -1,26 +1,29 @@
 $(function() {
 
+$('#SUBMIT').click(function() {
+  $('#new-post').show()
 });
-//When you click on submit button, creates new item for the list
 // listen for a submit event on a form
 $('#new-post').on('submit', function(e) {
-	e.preventDefault() // do not refresh the page
-  	var nextTitle = $(link1).val();
-  	var url = $(link2).val()
+  e.preventDefault() // do not refresh the page
+    var nextTitle = $(link1).val();
+    var url = $(link2).val()
       $(link1).val('')
       $(link2).val('')
 
   if (nextTitle.length === 0) return 0
   if (url.length === 0) return 0
   
-
+  $('#new-post').slideUp()
   $('ol').append('<li><span><i class="fa fa-star-o" aria-hidden="true"></i></span>' + " " +  nextTitle + ' ' + '(' + url + ')' + "</li>") 
+
+  //$('ul').append('<li>' + nextTitle + ' ' + '(' + url + ')' + "</li>") 
 $('span').on('click',function(e){
-	$(this).find($(".fa")).toggleClass('fa fa-star-o fa fa-star');
-	e.stopPropogation()
+  $(this).children().toggleClass('fa fa-star-o fa fa-star');
+  e.stopPropogation()
+
+  });
+
+
 });
-
 });
-
-
-
